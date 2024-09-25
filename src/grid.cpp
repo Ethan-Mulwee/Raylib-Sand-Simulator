0c9b06dee6d240133a8820898613fec245574363
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-09-24 21:32:11 -0400

    works

--- src/grid.cpp
+++ src/grid.cpp
@@ -3,12 +3,17 @@
 #include "stdio.h"
 #include "string"
 
-void Grid::Draw(){
-    int SandCount = 0;
-    int WaterCount = 0;
+void Grid::Step() {
     for (int row = 0; row < rows; row++) {
         for (int column = 0; column < columns; column++) {
-            cells[row][column]->update(&cells, row, column);
+            cells[row][column]->update(this, row, column);
+        }
+    }
+}
+
+void Grid::Draw(){
+    for (int row = 0; row < rows; row++) {
+        for (int column = 0; column < columns; column++) {
             if (!cells[row][column]->empty) DrawRectangle(column*cellSize, row*cellSize, cellSize, cellSize,cells[row][column]->color);
         }
     }
@@ -21,49 +26,8 @@ void Grid::Set(int row, int column, std::shared_ptr<Cell> celltype) {
     }
 }
 
-void Grid::Step() {
 
-}
-
-// bool Grid::CheckEmpty(int row, int rowOffset, int column, int columnOffset)
-// {
-//     if (column+columnOffset < columns && column+columnOffset >= 0 && row + rowOffset < rows && row+rowOffset >= 0) {
-//         return cells[row + rowOffset][column + columnOffset].state == EMPTY && !cells[row][column].updated && !cells[row+rowOffset][column+columnOffset].updated;
-//     }
-//     return false;
-// }
-
-// bool Grid::CheckCell(int row, int rowOffset, int column, int columnOffset, Grid::State stateInput)
-// {
-//     if (column+columnOffset < columns && column+columnOffset >= 0 && row + rowOffset < rows && row+rowOffset >= 0) {
-//         return cells[row + rowOffset][column + columnOffset].state == stateInput && !cells[row][column].updated && 2 > cells[row+rowOffset][column+columnOffset].updated;
-//     }
-//     return false;
-// }
-
-// //Return true if in bounds and false if out of bounds
-// bool Grid::MoveCell(int row, int rowOffset, int column, int columnOffset)
-// {
-//     if (column+columnOffset < columns && column+columnOffset >= 0 && row + rowOffset < rows && row+rowOffset >= 0) {
-//         cells[row + rowOffset][column + columnOffset].color = cells[row][column].color;
-//         cells[row + rowOffset][column + columnOffset].state = cells[row][column].state;
-//         cells[row + rowOffset][column + columnOffset].updated = 1;
-//         cells[row][column].state = EMPTY;
-//         return true;
-//     }
-//     return false;
-// }
-
-// bool Grid::SwapCell(int row1, int row2, int column1, int column2) {
-//     Cell cell1 = cells[row1][column1];
-//     Cell cell2 = cells[row2][column2];
-//     cell1.updated += 1;
-//     cell2.updated += 1;
-//     cells[row1][column1] = cell2;
-//     cells[row2][column2] = cell1;
-// }
-
-void Grid::Sand::update(std::vector<std::vector<std::shared_ptr<Cell>>>* cells, int row, int column)
+void Grid::Sand::update(Grid* grid, int row, int column)
 {
-    if ((*cells)[row+1][column]->empty) (*cells)[row+1][column] = (*cells)[row][column];
+    (*grid).cells[row+1][column]->color = RED;
 }
--- src/grid.hpp
+++ src/grid.hpp
@@ -8,34 +8,25 @@ class Grid {
         class Cell
         {
             public:
+            Cell() {}
+            virtual ~Cell() {}
+            Cell(Color colorI, bool emptyI) {color = colorI; empty = emptyI;}
             Color color = Color{0,0,0,255};
             bool empty = false;
 
-            virtual ~Cell() {}
-            Cell() {}
-            Cell(Color colorI, bool emptyI) {color = colorI; empty = emptyI;}
-
-            virtual void update(std::vector<std::vector<std::shared_ptr<Cell>>>* cells, int row, int column) {
-                
-            }
+            virtual void update(Grid* grid, int row, int column) {}
         };
 
         class Sand : public Cell {
             public:
             Sand(Color colorI) {color = colorI;}
-            void update(std::vector<std::vector<std::shared_ptr<Cell>>>* cells, int row, int column);
+            void update(Grid* grid, int row, int column);
         };
         Grid(int rows, int columns, int cellSize)
         : rows(rows), columns(columns), cellSize(cellSize), cells(rows, std::vector<std::shared_ptr<Cell>>(columns, std::make_shared<Cell>(BLACK, true))){};
         void Draw();
         void Set(int row, int column, std::shared_ptr<Cell> celltype);
         void Step();
-        // bool CheckEmpty(int row, int rowOffset, int column, int columnOffset);
-        // bool MoveCell(int row, int rowOffset, int column, int columnOffset);
-        // bool SwapCell(int row1, int row2, int column1, int column2);
-        // bool CheckCell(int row, int rowOffset, int column, int columnOffset, Grid::State stateInput);
-
-    private:
         int rows;
         int columns;
         int cellSize;

8cb5c7cc397090433a1c9c77edd4152d1a2bb355
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-09-24 22:52:36 -0400

    AAAAA

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,18 +1,18 @@
-#include "grid.hpp"
+#include "simulation.hpp"
 #include "raylib.h"
 #include "stdio.h"
 #include "string"
 
 
-void Grid::Step() {
+void Step() {
     for (int row = rows-1; row >= 0; row--) {
         for (int column = 0; column < columns; column++) {
-            cells[row][column]->update(this, row, column);
+            cells[row][column]->update(row, column);
         }
     }
 }
 
-void Grid::Draw(){
+void Draw(){
     for (int row = 0; row < rows; row++) {
         for (int column = 0; column < columns; column++) {
             if (!cells[row][column]->empty) DrawRectangle(column*cellSize, row*cellSize, cellSize, cellSize,cells[row][column]->color);
@@ -20,7 +20,7 @@ void Grid::Draw(){
     }
 }
 
-void Grid::Set(int row, int column, std::shared_ptr<Cell> celltype) {
+void Set(int row, int column, std::shared_ptr<Cell> celltype) {
     //if within bounds
     if (row < rows && row > -1 && column < columns && column > -1) {
         cells[row][column] = celltype;
@@ -28,10 +28,10 @@ void Grid::Set(int row, int column, std::shared_ptr<Cell> celltype) {
 }
 
 
-void Grid::Sand::update(Grid* grid, int row, int column)
+void Sand::update(int row, int column)
 {
-    if (row+1 == (*grid).rows-1) return;
-    if ((*grid).cells[row+1][column]->empty) {
-        (*grid).Set(row+1, column, std::make_shared<Grid::Sand>(WHITE));
+    if (row+1 == rows-1) return;
+    if (cells[row+1][column]->empty) {
+        Set(row+1, column, std::make_shared<Sand>(WHITE));
     }
 }
--- src/simulation.hpp
+++ src/simulation.hpp
@@ -3,49 +3,34 @@
 #include <memory>
 #include "raylib.h"
 
-class Grid {
-    public:
-        class Cell
-        {
-            public:
-            Cell() {}
-            virtual ~Cell() {}
-            Cell(Color colorI, bool emptyI) {color = colorI; empty = emptyI;}
-            Color color = Color{0,0,0,255};
-            bool empty = false;
+std::vector<std::vector<std::shared_ptr<Cell>>> cells;
+int rows;
+int columns;
+int cellSize;
 
-            virtual void update(Grid* grid, int row, int column) {}
-        };
-
-        class Sand : public Cell {
-            public:
-            Sand(Color colorI) {color = colorI;}
-            void update(Grid* grid, int row, int column);
-        };
-        std::vector<std::vector<std::shared_ptr<Cell>>> cells;
-        Grid(int rows, int columns, int cellSize)
-        : rows(rows), columns(columns), cellSize(cellSize), cells(rows, std::vector<std::shared_ptr<Cell>>(columns, std::make_shared<Cell>(BLACK, true))){};
-        void Draw();
-        void Set(int row, int column, std::shared_ptr<Cell> celltype);
-        void Step();
-        void Swap();
-        int rows;
-        int columns;
-        int cellSize;
+void IntializeGrid(int rows, int columns) {
+    cells = std::vector<rows, std::vector<columns, std::make_shared<Cell>(BLACK, true)>>
 };
 
-// class Behavior {
-//     public:
-//     Behavior() {}
-//     virtual void update() {
+class Cell
+{
+    public:
+    Cell() {}
+    virtual ~Cell() {}
+    Cell(Color colorI, bool emptyI) {color = colorI; empty = emptyI;}
+    Color color = Color{0,0,0,255};
+    bool empty = false;
 
-//     }
-// };
+    virtual void update(int row, int column) {}
+};
 
-// class GoesDown : public Behavior {
-//     public:
-//     GoesDown() {}
-//     void update() {
+class Sand : public Cell {
+    public:
+    Sand(Color colorI) {color = colorI;}
+    void update(int row, int column);
+};
 
-//     }
-// };
+void Draw();
+void Set(int row, int column, std::shared_ptr<Cell> celltype);
+void Step();
+void Swap();
